b62a7bedd51ac716038d80a16ce30df46e627dc6
Falcon-syz <sunyzh9527@sina.com>
2021-08-04 15:18:11 +0800

    sunyzh init

---
+++ pages/make/make.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		make
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
---
+++ pages/open/open.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		open
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
